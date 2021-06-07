22958cb4230de49cb58ee9eb91bcaf2089848516
Marcela <marcelasmoura@gmail.com>
2021-06-07 18:51:29 -0300

    Add roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: [:super_admin, :client_admin, :client]
+
+  before_create :set_admin_role!
+
+  private
+
+  def set_admin_role!
+    if email.include? '@paynow.com.br'
+      self.role = :super_admin
+    else
+      if User.where('email like ?', '%@codeplay.com.br').any?
+        self.role = :client
+      else
+        self.role = :client_admin
+      end
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+  context 'Create user roles according the email domain' do
+    it 'if email domain belongs to paynow' do
+      user = User.create!(email:'user@paynow.com.br', 
+                   password:'thisisapassword', 
+                   password_confirmation: 'thisisapassword')
+
+      expect(user).to be_super_admin
+    end
+
+    it 'email domain belong to codeplay as a first registered' do
+      user = User.create!(email:'user@codeplay.com.br', 
+                   password:'thisisapassword', 
+                   password_confirmation: 'thisisapassword')
+
+      expect(user).to be_client_admin
+    end
+
+    it 'email domain belong to codeplay' do
+     User.create!(email:'user@codeplay.com.br', 
+                   password:'thisisapassword', 
+                   password_confirmation: 'thisisapassword')
+
+      user = User.create!(email:'user2@codeplay.com.br', 
+                   password:'thisisapassword', 
+                   password_confirmation: 'thisisapassword')
+
+      expect(user).to be_client
+    end
+
+  end
 end
